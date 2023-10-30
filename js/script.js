74efd3c855f9cb23d28314dd6ca5809d144ae0a2
Paulo Henrique <paulo24041996@gmail.com>
2023-10-29 18:14:54 -0700

    Escape Room Front End

--- js/script.js
+++ js/script.js
@@ -7,15 +7,17 @@ let tentativas = 0;
 function Jogar() {
     let input = parseInt(document.querySelector("#number").value); // Obtenha o valor do input dentro da função
     tentativas += 1;
-    if (input < numero && tentativas <= 5) {
+    if (input < numero && tentativas < 5) {
         alert("Tente um número maior...");
-    } else if (input > numero && tentativas <= 5) {
+    } else if (input > numero && tentativas < 5) {
         alert("Tente um número menor...");
-    } else if (tentativas === 5) {
+    } else if (tentativas == 5 && input != numero) {
         // Exibir a mensagem final após o término do jogo
-        resultado.textContent = ("Você está morto!!! O Guardião dos portais infernais te matou com seus poderes mágicos e as Succubus e os Cães do Inferno comeram sua carne vivo. GAME OVER!!! O seu número da morte é: " + numero);
+        alert ("Você está morto!!! O Guardião dos portais infernais te matou com seus poderes mágicos e as Succubus e os Cães do Inferno comeram sua carne vivo. GAME OVER!!! O seu número da morte é: " + numero);
+        location.reload();
     } else {
         // Exibir a mensagem final após o término do jogo
-        resultado.textContent ="Parabéns. O seu inimigo está morto e você conseguiu vencer o Guardião dos Portais Infernais. O seu número da sorte é: " + numero;
-    }        
+        alert("Parabéns. O seu inimigo está morto e você conseguiu vencer o Guardião dos Portais Infernais. O seu número da sorte é: " + numero);
+        location.reload();
+    }       
     }
