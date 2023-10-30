085a246c2a2b88ab6475e0e068462cad7a6d8139
Paulo Henrique <paulo24041996@gmail.com>
2023-10-29 17:31:12 -0700

    Escape Room Front-End

--- js/script.js
+++ js/script.js
@@ -14,22 +14,17 @@ function Jogar() {
     } else if (input > numero && tentativas < 5) {
         alert("Tente um número menor...");
     } else if (tentativas === 5) {
-        alert("Você está morto!!! O Guardião dos portais infernais te matou com seus poderes mágicos e as Succubus e os Cães do Inferno comeram sua carne vivo. GAME OVER!!! O seu número da morte é: " + numero)
-        // Exibir uma caixa de diálogo perguntando se deseja continuar
-        const continuar = confirm("Deseja continuar?");
-        if (continuar) {
-            location.reload(); // Recarrega a página
-        } else {
-            window.close(); // Fecha a janela
-        }
+        // Exibir a mensagem final após o término do jogo
+        alert("Você está morto!!! O Guardião dos portais infernais te matou com seus poderes mágicos e as Succubus e os Cães do Inferno comeram sua carne vivo. GAME OVER!!! O seu número da morte é: " + numero);
     } else {
+        // Exibir a mensagem final após o término do jogo
         alert("Parabéns. O seu inimigo está morto e você conseguiu vencer o Guardião dos Portais Infernais. O seu número da sorte é: " + numero);
-        // Exibir uma caixa de diálogo perguntando se deseja continuar
-        const continuar = confirm("Deseja continuar?");
-        if (continuar) {
-            location.reload(); // Recarrega a página
-        } else {
-            window.close(); // Fecha a janela
-        }
     }
+            // Exibir uma caixa de diálogo perguntando se deseja continuar
+            const continuar = confirm("Deseja continuar?");
+            if (continuar) {
+                location.reload(); // Recarrega a página
+            } else {
+                window.close(); // Fecha a janela
+            }
 }
