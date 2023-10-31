67dced60a35bb2cf75607d48215a43c3bd360e8b
Paulo Henrique <paulo24041996@gmail.com>
2023-10-30 18:19:46 -0700

    Escape Room Front-End

---
+++ js/resultado.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let sim = document.getElementById("sim");
+    let não = document.getElementById("nao");
+
+    sim.addEventListener("click", Continuar);
+    não.addEventListener("click", function() {
+        const confirmClose = window.confirm("Deseja realmente fechar a janela?");
+        if (confirmClose) {
+            alert("A janela não pode ser fechada automaticamente por motivos de segurança. Por favor, feche-a manualmente usando os controles do navegador.");
+        }
+    });
+    
+});
+
+function Continuar() {
+    window.location.href = "http://127.0.0.1:5500/jogar.html";
+}
--- js/script.js
+++ js/script.js
@@ -1,27 +1,34 @@
-let button = document.querySelector("#btn");
-let audio = new Audio("audio/Satans Lament.mp3");
-button.addEventListener("click", function (){
-    audio.play();
-    Jogar();
-});
-let resultado = document.querySelector("#resultado");
-let numero = Math.floor(Math.random() * 100) + 1; // Gere um número aleatório entre 1 e 100
-let tentativas = 0;
+document.addEventListener("DOMContentLoaded", function() {
+    let sortedNum = Math.floor(Math.random() * 100) + 1; // Gera número Aleatório
+    let jogar = document.getElementById("jogar");
+    let sim = document.getElementById("sim");
+    let não = document.getElementById("nao");
+    let tentativas = 0; // Numero de Tentativas
 
-function Jogar() {
-    let input = parseInt(document.querySelector("#number").value); // Obtenha o valor do input dentro da função
-    tentativas += 1;
-    if (input < numero && tentativas < 5) {
-        alert("Tente um número maior...");
-    } else if (input > numero && tentativas < 5) {
-        alert("Tente um número menor...");
-    } else if (tentativas == 5 && input != numero) {
-        // Exibir a mensagem final após o término do jogo
-        alert ("Você está morto!!! O Guardião dos portais infernais te matou com seus poderes mágicos e as Succubus e os Cães do Inferno comeram sua carne vivo. GAME OVER!!! O seu número da morte é: " + numero);
-        location.reload();
-    } else {
-        // Exibir a mensagem final após o término do jogo
-        alert("Parabéns. O seu inimigo está morto e você conseguiu vencer o Guardião dos Portais Infernais. O seu número da sorte é: " + numero);
-        location.reload();
-    }       
+    jogar.addEventListener("click", Jogar);
+    sim.addEventListener("click", Continuar);
+    não.addEventListener("click", Encerrar);
+
+    function Jogar() {
+        let userNum = parseInt(document.getElementById("number").value);
+        tentativas += 1;
+
+        if (userNum == sortedNum) {
+            window.location.href = "http://127.0.0.1:5500/resultado.html";
+        } else if (tentativas === 5) {
+            window.location.href = "http://127.0.0.1:5500/perdeu.html";
+        } else if (userNum < sortedNum) {
+            alert("Tente um número maior...");
+        } else {
+            alert("Tente um número menor...");
+        }
     }
+
+    function Continuar() {
+        window.location.href = "http://127.0.0.1:5500/jogar.html";
+    }
+
+    function Encerrar() {
+        window.close();
+    }
+});
